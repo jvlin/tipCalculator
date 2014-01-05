e6e04b35b2f961bb8896cad34c0c1425ed2bcfa9
Joey Lin <jvlin@yahoo-inc.com>
2014-01-05 14:24:55 -0800

    first commit

---
+++ tipcalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tipcalculator
+//
+//  Created by Joey Lin on 1/5/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Joey Lin on 1/4/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
